9a84ed0e9e60051067deb8832aa3404b9255104e
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-10 00:04:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,7 +7,7 @@ def array_with_two_elements
 end
 
 def first_element(array)
-  array.first
+  array[0]
 end
 
 def third_element(array)
@@ -19,4 +19,13 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.count
+end
